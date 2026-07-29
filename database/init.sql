88de7865cd2cc0a04453621a10bd37888982d6d0
ElynRenoirte <elyn.renoirte@student.ehb.be>
2026-07-29 23:16:48 +0200

    Docker runs

---
+++ database/init.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    name VARCHAR(100),
+
+    email VARCHAR(100)
+
+);
+
+CREATE TABLE reports (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    user_id INT,
+
+    description TEXT,
+
+    latitude DOUBLE,
+
+    longitude DOUBLE,
+
+    direction VARCHAR(50),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY(user_id)
+        REFERENCES users(id)
+
+);
