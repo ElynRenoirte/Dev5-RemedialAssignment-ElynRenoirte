a430deb1f3361c97f531ea42e6a4e2af132c740a
ElynRenoirte <elyn.renoirte@student.ehb.be>
2026-07-31 15:50:48 +0200

    project setup with docker and mysql

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
+require("./config/database");
+
 const express = require("express");
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Backend is running!");
+	res.send("Backend is running!");
 });
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+	console.log("Server running on port 3000");
 });
